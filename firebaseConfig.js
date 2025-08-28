b9f767f019ae996fa8807f369d79b81a4362048d
faithninaallego <faitheey343@gmail.com>
2025-08-28 11:42:59 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXo4Qj4rnOs3ls4fcSVrZ7Vob4_FAIgss",
+  authDomain: "fna-goal-tracker.firebaseapp.com",
+  projectId: "fna-goal-tracker",
+  storageBucket: "fna-goal-tracker.firebasestorage.app",
+  messagingSenderId: "784677545590",
+  appId: "1:784677545590:web:c972a2e435f617ffa56157"
+};
+
+const app = initializeApp(firebaseConfig);
